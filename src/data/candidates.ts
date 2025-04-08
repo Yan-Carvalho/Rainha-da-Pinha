de6c89179fc7d88613392c72feaf59d70f5e4288
Yan <76734082+Yan-Carvalho@users.noreply.github.com>
2025-04-08 08:49:19 -0300

    Update candidates.ts images

--- src/data/candidates.ts
+++ src/data/candidates.ts
@@ -2,41 +2,41 @@ export const candidates = [
   {
     name: "Bianca Gabriela",
     city: "18 anos",
-    photo: "/src/images/Bianca.png",
+    photo: "https://i.ibb.co/xtSxfLsy/Bianca.png",
     instagram:
       "https://www.instagram.com/biancagabrielly296?igsh=eTZnbjdocWM4ODNh",
   },
   {
     name: "Maria Eduarda",
     city: "20 anos",
-    photo: "/src/images/MariaEduarda.png",
+    photo: "https://i.ibb.co/hFHcrgyT/Maria-Eduarda.png",
     instagram:
       "https://www.instagram.com/duda.lbrito?igsh=MXNqeTN3OGZ0cXBldg==",
   },
   {
     name: "Maria Clara",
     city: "16 anos",
-    photo: "/src/images/MariaClara.png",
+    photo: "https://i.ibb.co/Mxw9gpBk/Maria-Clara.png",
     instagram:
       "https://www.instagram.com/mariaclararochasil807?igsh=MWZuemJocnUxMHhnOA==",
   },
   {
     name: "Carla Cristina",
     city: "23 anos",
-    photo: "/src/images/Carla.png",
+    photo: "https://i.ibb.co/70YfcGw/Carla.png",
     instagram:
       "https://www.instagram.com/carlacristinats?igsh=MWU0M290bm9iYTdsNw==",
   },
   {
     name: "Lívia Gomes",
     city: "20 anos",
-    photo: "/src/images/Livia.png",
+    photo: "https://i.ibb.co/5NYrwMW/Livia.png",
     instagram: "https://www.instagram.com/livhiaax?igsh=YW15dmozcThzbzdr",
   },
   {
     name: "Mirelle Silva",
     city: "16 anos",
-    photo: "/src/images/Mirelle.png",
+    photo: "https://i.ibb.co/qYk66y8h/Mirelle.png",
     instagram:
       "https://www.instagram.com/mirelle_silva_0?igsh=dG5kZnNvZHJ6ZGg4",
   },
